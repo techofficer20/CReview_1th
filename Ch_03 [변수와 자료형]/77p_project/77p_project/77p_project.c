2d8b95db88bf1900d555081b9f2bdafd7b03e6e8
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-05 12:59:20 +0900

    정수형 사용하기 (2)

---
+++ Ch_03 [변수와 자료형]/77p_project/77p_project/77p_project.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	short a = 32766;
+	short b = 10;
+	short c = a + b;
+	int d = a + b;
+
+	printf("정수형 a의 값 %d와 b의 값 %d의 합은 %d 이다.\n", a, b, c); // 범위 벗어남
+	printf("정수형 a의 값 %d와 b의 값 %d의 합은 %d 이다.\n", a, b, d);
+
+	return 0;
+}
