28abe4e96ab619e7b0ac8979bd80fe7b94e7ad8b
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-05 13:16:03 +0900

    사용자로부터의 데이터 입력 (2)

---
+++ Ch_03 [변수와 자료형]/92p_project/92p_project/92p_project.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int input1, input2;
+	int total;
+
+	printf("두 개의 정수를 입력하세요: ");
+	scanf("%d%d", &input1, &input2);
+	total = input1 + input2;
+
+	printf("두 수의 합은 %d 이다.\n", total);
+
+	return 0;
+}
