ec787575f645950c474ea64ac31147396c23d893
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-05 13:19:05 +0900

    92p 과제 3-2

---
+++ CAssignment/3-2/3-2/3-2.c
@@ -0,0 +1,20 @@
+﻿/*
+사용자로부터 3개의 정수를 입력 받아서, 3개의 수를 곱한 결과를 출력하라.
+출력 예)
+세 개의 정수를 입력 하세요: 2 3 4
+세 수의 곱은 24 이다.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	int result;
+	printf("세 개의 정수를 입력 하세요: ");
+	scanf("%d %d %d", &a, &b, &c);
+	result = a * b * c;
+	printf("세 수의 곱은 %d 이다.\n", result);
+
+	return 0;
+}
