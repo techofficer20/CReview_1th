d2c1b7ec0b3586b3c8cd61c46ccd6147e4fc2fc3
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-06 11:02:52 +0900

    증감 연산자

---
+++ Ch_04 [연산자]/102p_project/102p_project/102p_project.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 5;
+	printf("++a의 처리 결과 : %d\n", ++a);
+	
+	a = 5;
+	printf("a++의 처리 결과 : %d\n", a++);
+	printf("a를 다시 출력: %d\n\n", a);
+
+	b = 10;
+	printf("--b의 처리 결과 : %d\n", --b);
+
+	b = 10;
+	printf("b--의 처리 결과 : %d\n", b--);
+	printf("b를 다시 출력 : %d\n\n", b);
+
+	return 0;
+}
