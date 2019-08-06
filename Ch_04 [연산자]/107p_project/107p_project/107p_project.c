448ac174a2fda80107198fbbce31434393bd4f5e
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-06 11:11:17 +0900

    논리 연산자

---
+++ Ch_04 [연산자]/107p_project/107p_project/107p_project.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	int b = 11;
+	int c = 12;
+	int d = 10;
+
+	printf("!(a == 10) 의 결과는 : %d\n", !(a == 10)); // 값 반대
+	printf("(a < b) && (a < c) 의 결과는 : %d\n", (a < b) && (a < c)); // 모두 true면 true
+	printf("(a >= c) || (a == d) 의 결과는 : %d\n", (a >= c) || (a == d)); // 하나 이상 true면 true
+
+	return 0;
+}
