06469d938bd7877c5400235526d941242393b552
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-06 10:52:33 +0900

    산술 연산자

---
+++ Ch_04 [연산자]/98p_project/98p_project/98p_project.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int  a = 5;
+	int b = 3;
+	
+	printf("두 수의 합은 %d 입니다.\n", a + b);
+	printf("두 수의 차는 %d 입니다.\n", a - b);
+	printf("두 수의 곱은 %d 입니다.\n", a * b);
+	printf("두 수를 나누기는 %d 입니다.\n", a / b);
+	printf("두 수의 나머지는 %d 입니다.\n", a % b);
+
+	return 0;
+}
