48165343807dc86147db4e4796a7b28f0461ff38
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-06 11:06:38 +0900

    관계 연산자

---
+++ Ch_04 [연산자]/105p_project/105p_project/105p_project.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	int b = 11;
+	int c = 12;
+	int d = 10;
+
+	// 0: false, 1: true
+	printf("a == b 의 결과는 : %d\n", a == b);
+	printf("a < b 의 결과는 : %d\n", a < b);
+	printf("a > c 의 결과는 : %d\n", a > c);
+	printf("a == d 의 결과는 : %d\n", a == d);
+
+	return 0;
+}
