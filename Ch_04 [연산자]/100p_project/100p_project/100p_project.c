a38d6dd16eae0cc69ae713f8760e6e83e1bb6fad
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-06 10:59:14 +0900

    대입 연산자

---
+++ Ch_04 [연산자]/100p_project/100p_project/100p_project.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	int b = 3;
+
+	a += b;
+	printf("a += b의 결과는 %d 입니다.\n", a);
+	a -= b;
+	printf("a -= b의 결과는 %d 입니다.\n", a);
+	a *= b;
+	printf("a *= b의 결과는 %d 입니다.\n", a);
+	a /= b;
+	printf("a /= b의 결과는 %d 입니다.\n", a);
+	a %= b;
+	printf("a %= b의 결과는 %d 입니다.\n", a);
+
+	return 0;
+}
