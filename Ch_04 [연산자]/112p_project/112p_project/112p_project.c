fcd7691dc8bc8c7b2149a4039b5651d9c78e8045
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-15 10:00:00 +0900

    비트 연산자
    & 연산자

---
+++ Ch_04 [연산자]/112p_project/112p_project/112p_project.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	int a = 74;
+	int b = 19;
+
+	printf("a&b의 연산 결과는 : %x\n", a & b); // & 연산자: 두 개의 비트가 모두 1일 때 1을 리턴, 서로 다르거나 모두 0일 때 0을 리턴
+
+	return  0;
+}
