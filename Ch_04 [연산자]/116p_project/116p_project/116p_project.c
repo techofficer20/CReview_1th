ffd041c67c09fb16bf17a4288e32f3b4f991d94e
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-20 23:44:36 +0900

    비트 연산자
    쉬프트(Shift)연산자

---
+++ Ch_04 [연산자]/116p_project/116p_project/116p_project.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+	int b = 74;
+
+	printf("a<<2의 연산 결과는: %x\n", a << 2); // 왼쪽으로 2비트만큼 이동한 값을 리턴함.
+	printf("b<<2의 연산 결과는: %x\n", b << 2);
+
+	return 0;
+}
