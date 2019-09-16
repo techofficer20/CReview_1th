f786e9414186be5f455aec1f1725462ca4710d85
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-09-16 21:41:31 +0900

    연산의 우선순위와 결합 순서

---
+++ Ch_04 [연산자]/120p_project/120p_project/120p_project.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	int a = 4;
+	a = a * 2 + 6 / 2 - 1;
+	printf("첫 번째 연산 결과는 : %d\n", a);
+
+	a = 4;
+	a *= 2 + 6 / 2 - 1;
+	printf("두 번째 연산 결과는 : %d\n", a);
+
+	return 0;
+}
